deb6bd3c7c45c54852d71a8a0db9a5771343413e
Jonathan Schmitt <jonathan.schmitt@theorangeforce.us>
2025-02-14 09:20:25 -0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -23,34 +23,35 @@ function App() {
           me, cuddle with me, or spend time with me. I don't like to be
           restricted, so please be mindful when interacting with me.
         </p>
-        <h3>I love to adventure 🌳</h3>
+        <h3>I love to complain 😭</h3>
+        <p>
+          Sometimes I might get lost or scared and scream about it (I can be
+          very loud). If I seem stressed, please help me get somewhere safe, and
+          inform my parents at the number on my tag. Please do not feed or
+          shelter me, I can return home or my parents can locate me to pick me
+          up.
+        </p>
+        <h3>I love to adventure ⛰️</h3>
         <p>
           The only thing I love more than yelling and cuddlig is splorin'. I
           love the great outdoors, even in intense weather conditions I might
           choose to go out. If you're worried or see me in danger please contact
           my parents at the number on my tag.
         </p>
-        <h3>I'm safe and healthy⚕️</h3>
+        <h3>I'm safe and healthy ⚕️</h3>
         <p>
           I am up to date on my shots and well taken care of, please do not be
-          worried about my health of safety. I came from a foster home in Sain
-          Louis in 2022, and have lived in Denver since.
+          worried about my health or safety. I came from a foster home in Saint
+          Louis in 2022, and have lived as an indoor-outdoor cat in Denver
+          since.
         </p>
-        <h3>I love to complain 😭</h3>
+        <h3>I'm kind of a stinker 😼</h3>
         <p>
-          Sometimes I might get lost or scared and scream about it (I can be
-          very loud). If you find me and I seem stressed, please help me get
-          somewhere safe, and inform my parents at the number on my tag. Please
-          do not feed or shelter me, I can return home or my parents can locate
-          me to pick me up.
-        </p>
-        <h3>I'm kind of a stinker! 😼</h3>
-        <p>
-          Despite all of my adorable qualities, I'm kindof a stinker. I have
+          Despite all of my adorable qualities, I'm kindof a stinker! I have
           been known to be loud, bother people until they let me into their
-          homes, not return to my home when I'm supposed to, claw things that
-          I'm not supposed to, and generally be a butt. Please text my parents
-          at the number listed on my tag if I've caused you any trouble.
+          homes, not return to my home when I'm supposed to, jump on things,
+          claw things, and generally be a butt. Please text my parents at the
+          number listed on my tag if I've caused you any trouble.
         </p>
         <h3>Identifying me 🕵️</h3>
         <p>
